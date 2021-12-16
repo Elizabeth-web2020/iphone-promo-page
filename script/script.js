e0a62b85c26200b17857ee4c7d48418cb67242b8
Elizabeth Popova <elizabethpopova2020@gmail.com>
2021-12-16 18:56:09 +0100

    fixed the accordion function

--- script/script.js
+++ script/script.js
@@ -50,7 +50,41 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
 
-    const accordion = () => {}
+    const accordion = () => {
+        const characteristicsListElem = document.querySelector('.characteristics__list');
+        const characteristicsItemElems = document.querySelectorAll('.characteristics__item');
+
+        const open = (button, dropDown) => {
+            closeAllDrops(button, dropDown);
+            dropDown.style.height = `${dropDown.scrollHeight}px`;
+            button.classList.add('active');
+            dropDown.classList.add('active');
+        };
+
+        const close = (button, dropDown) => {
+            button.classList.remove('active');
+            dropDown.classList.remove('active');
+            dropDown.style.height = '';
+        };
+
+        const closeAllDrops = (button, dropDown) => {
+            characteristicsItemElems.forEach((elem) => {
+                if (elem.children[0] !== button && elem.children[1] !== dropDown) {
+                    close(elem.children[0], elem.children[1]);
+                }
+            })
+        };
+
+        characteristicsListElem.addEventListener('click', (e) => {
+            const target = e.target;
+            if (target.classList.contains('characteristics__title')) {
+                const parent = target.closest('.characteristics__item');
+                const description = parent.querySelector('.characteristics__description');
+
+                description.classList.contains('active') ? close(target, description) : open(target, description);
+            }
+        });
+    };
 
 
     tabs();
